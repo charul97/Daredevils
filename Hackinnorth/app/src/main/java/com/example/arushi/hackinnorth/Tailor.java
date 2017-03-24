9a7d0766e7534f3794f4842d8c27387bd7ff302f
charul97 <charulagrawal97@gmail.com>
2017-03-24 10:12:23 +0530

    Initial commits

---
+++ Hackinnorth/app/src/main/java/com/example/arushi/hackinnorth/Tailor.java
@@ -0,0 +1,13 @@
+package com.example.arushi.hackinnorth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tailor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tailor);
+    }
+}
